a5762937be8f4b59d30fa612e2eb86120448bf7f
Impre-visible <romeo@chevrier.dev>
2025-05-24 08:39:07 +0200

    ✨ feat: add JWT middleware configuration and exclude specific routes from authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 
 import { ConfigModule } from '@nestjs/config';
@@ -8,14 +8,25 @@ import { AppService } from './app.service';
 
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { JwtMiddleware } from './middleware/JwtMiddleware';
+import { MeModule } from './modules/me/me.module';
 
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: ['.env'],
-  }), HttpModule, AuthModule],
+  }), HttpModule, AuthModule, MeModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleware)
+      .exclude(
+        { path: 'api/auth/login', method: RequestMethod.POST },
+        { path: 'api/auth/register', method: RequestMethod.POST },
+        { path: 'api/auth/refresh', method: RequestMethod.POST },
+      ).forRoutes('*');
+  }
+}
